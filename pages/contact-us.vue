6141edc11d1bf6dd2412f3cbd73230277fd706af
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-05-15 15:05:33 +0000

    WIP: Add contact page

---
+++ pages/contact-us.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Nous contacter - Input Studio',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+}
+</style>
