f4fe241a1ae46c33a9ecdcd923eae591a8c161b8
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-05-15 14:48:16 +0000

    Add teammate cards

--- components/HomeTeam.vue
+++ components/HomeTeam.vue
@@ -1,9 +1,63 @@
 <template>
-  <div id="team" class="team--wrapper"></div>
+  <div id="team" class="team--wrapper">
+    <h1 class="team--title">Faire connaissance avec l'équipe</h1>
+
+    <div class="team--grid">
+      <HomeTeamCard
+        v-for="(teammate, index) in team"
+        :key="index"
+        :name="teammate.name"
+        :bio="teammate.bio"
+        :image="teammate.image"
+        :social="teammate.social"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      team: [
+        {
+          name: 'Cheick Sinoussou',
+          bio: 'UI/UX Designer...',
+          image: 'https://ui-avatars.com/api/?name=C+S&size=256',
+          social: '',
+        },
+        {
+          name: 'Henri-Joel Kouassi',
+          bio: 'Développeur Fullstack Flutter, Laravel et React...',
+          image: 'https://ui-avatars.com/api/?name=H+K&size=256',
+          social: '',
+        },
+        {
+          name: "Alex N'Guessan",
+          bio: 'Développeur fullstack Flutter, Node.js et Vue.js. Passionné de web design minimaliste. Amateur de café et grand fan de foot.',
+          image: 'https://ui-avatars.com/api/?name=A+N&size=256',
+          social: 'https://marcaureln.com',
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.team--wrapper {
+  width: 60vw;
+  padding: 3rem 0;
+  margin: auto;
+  margin-bottom: 6rem;
+}
+
+.team--title {
+  margin-bottom: 6rem;
+}
+
+.team--grid {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ components/HomeTeamCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="team--card--wrapper">
+    <img :src="image" :alt="'Photo de ' + name" />
+    <h2 class="name">
+      <a :href="social" target="_blank" rel="noopener noreferrer">
+        {{ name }}
+      </a>
+    </h2>
+    <p class="bio">{{ bio }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    social: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.team--card--wrapper {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+img {
+  width: 256px;
+  height: 256px;
+  border-radius: 50%;
+  margin-bottom: 1.5rem;
+  transition: opacity 0.1s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+
+.name {
+  margin-bottom: 1rem;
+  transition: color 0.1s;
+
+  &:hover {
+    color: #444;
+  }
+}
+
+.bio {
+  font-size: 1.2rem;
+  color: #333;
+  text-align: justify;
+}
+</style>
