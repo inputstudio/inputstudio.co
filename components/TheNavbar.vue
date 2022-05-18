ee6be0775dd04557d60cba09ab0e9be08a18e085
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-18 21:19:12 +0000

    Dropdown button added to navbar for mobile version

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -13,6 +13,19 @@
     <div class="navbar--end">
       <nuxt-link to="/contact-us" class="contact-btn">Nous contacter</nuxt-link>
     </div>
+
+    <div class="dropdown--menu">
+      <button class="dropdown--btn">
+        <ion-icon name="menu"></ion-icon>
+      </button>
+      <div class="dropdown--items">
+        <nuxt-link to="/">Accueil</nuxt-link>
+        <nuxt-link :to="{ path: '/', hash: 'services' }">Services</nuxt-link>
+        <nuxt-link :to="{ path: '/', hash: 'team' }">L'équipe</nuxt-link>
+        <nuxt-link to="/contact-us">Nous contacter</nuxt-link>
+      </div>
+    </div>
+    
   </nav>
 </template>
 
@@ -21,11 +34,32 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+
+$navbar-bg: #111; 
+
+.navbar--links, .navbar--end, .contact-btn {
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+}
+
+.dropdown--menu {
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+}
+
 .navbar--wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: #111;
+  background: $navbar-bg;
   color: white;
   padding: 1rem 20vw;
 }
@@ -51,5 +85,59 @@ export default {}
 
 .contact-btn {
   @include button($border: unset, $cursor: pointer);
+}
+
+.dropdown--btn {
+  display: flex;
+  justify-content: center;
+  background-color: $navbar-bg;
+  color: white;
+  padding: .5em;
+  border: unset;
+  position: relative;
+  cursor: pointer;
+  z-index: 1;
+
+  &:hover {
+    &::after {
+      opacity: 1;
+    }
+  }
+  
+  &::after{
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    opacity: 0;
+    z-index: -1;
+    transition: opacity 300ms;
+    background-color: lighten($navbar-bg, 5);
+  }
+
+  ion-icon {
+    font-size: 2em;
+  }
+}
+
+.dropdown--items {
+  background-color: $navbar-bg;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  z-index: 500;
+
+  a {
+    padding: .8em 1.5em;
+    text-align: center;
+    transition: background-color 300ms;
+
+    &:hover {
+      background-color: white;
+      color: $navbar-bg;
+    }
+  }
 }
 </style>
