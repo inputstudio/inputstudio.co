c94fca8b13025409bc59d4b053e8de659dd28e62
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-19 17:34:47 +0000

    Services card made

---
+++ components/HomeServiceCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="service--card--wrapper">
+    <div class="card--content">
+      <div class="card--header">
+        <ion-icon :name="icon_name"></ion-icon>
+        <h2 class="card--title">{{ title }}</h2>
+      </div>
+      <div class="card--body">
+        {{ description }}
+      </div>
+    </div>
+    <div class="card--footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon_name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.service--card--wrapper {
+  box-shadow: .047em .12em .80em rgba(128, 128, 128, 0.5) ;
+  
+}
+
+.card--content {
+  padding: 1em;
+}
+.card--header {
+  margin: .5em 0;
+
+  ion-icon {
+    font-size: 3em;
+  }
+}
+
+.card--title {
+  margin: 1em 0;
+}
+.card--body {
+  padding: 0 .5em;
+}
+</style>
--- components/HomeServices.vue
+++ components/HomeServices.vue
@@ -16,12 +16,50 @@
           fuga? Aliquam.
         </p>
       </div>
+    </div>
 
-      <div class="services--grid"></div>
+    <div class="services--grid">
+      <HomeServiceCard
+        v-for="(service, index) in services"
+        :key="index"
+        :icon_name="service.icon_name"
+        :title="service.title"
+        :description="service.description"
+      />
+
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      services: [
+        {
+          icon_name: 'globe',
+          title: 'Applications web',
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        },
+        {
+          icon_name: 'phone-portrait',
+          title: 'Applications mobiles',
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        },
+        {
+          icon_name: 'school',
+          title: 'Refactoring',
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        },
+        {
+          icon_name: 'albums',
+          title: 'Maquette',
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        },
+      ],
+    }
+  },
+}
+</script>
 <style lang="scss" scoped>
 .services--wrapper {
   width: 60vw;
@@ -49,5 +87,21 @@ p {
   font-size: 1.2rem;
   color: #333;
   margin-bottom: 1rem;
+}
+
+.services--grid {
+  @media screen and(max-width: 768px) {
+    display:flex;
+    flex-direction: column;
+  }
+  @media screen and(min-width: 768px) {
+    padding: 2em 0;
+    display: grid;
+    grid-template-columns: 48% 48%;
+  }
+
+  gap: 1em;
+  justify-content: center;
+  align-items: center;
 }
 </style>
