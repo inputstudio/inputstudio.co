523e1a1a02e18fd6a1d8e70e49cd2aeca0c24d10
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-22 01:59:31 +0000

    fade-in animation added to mixins

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -5,11 +5,11 @@
       <h3>Merci de nous contacter en remplissant correctement tous les champs ci-dessous.</h3>
       <div class="form--group">
         <label for="full_name">Nom et prénoms</label>
-        <input id="full_name" type="text" name="full_name">
+        <input id="full_name" type="text" name="full_name" />
       </div>
       <div class="form--group">
         <label for="email">Email</label>
-        <input id="email" type="email" name="email">
+        <input id="email" type="email" name="email" />
       </div>
       <div class="form--group">
         <label for="subject">Objet</label>
@@ -46,7 +46,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 .wrapper {
   @media screen and (max-width: 768px) {
     padding: 2em;
@@ -65,10 +64,12 @@ export default {
   flex-direction: column;
   gap: 1em;
 
-  input, textarea , select{
-    padding: .5em 1em;
-    border: .2em solid rgb(168, 168, 168);
-    border-radius: .3em;
+  input,
+  textarea,
+  select {
+    padding: 0.5em 1em;
+    border: 0.2em solid rgb(168, 168, 168);
+    border-radius: 0.3em;
   }
 
   select {
@@ -80,33 +81,42 @@ export default {
   }
 
   @media screen and (max-width: 768px) {
-    width: 100%; 
+    width: 100%;
   }
   @media screen and (min-width: 768px) {
     width: 60%;
   }
 }
 
-.form--group{
+.form--group {
   display: flex;
   flex-direction: column;
-  gap: .4em;
+  gap: 0.4em;
   width: 100%;
+  transform: translateX(-100%);
+
+  @for $i from 1 through 6 {
+    &:nth-child(#{$i}) {
+      animation: from-left 600ms 100ms * $i;
+      animation-fill-mode: forwards;
+    }
+  }
 }
 
 .row {
   display: flex;
   justify-content: space-between;
-  gap: .7em;
+  gap: 0.7em;
 }
 
 .btn--submit {
   @include button($border: solid, $cursor: pointer);
+  @include fade-in-animation
 }
 
 .icon--div {
   @media screen and (max-width: 768px) {
-    display: none; 
+    display: none;
   }
 
   @media screen and (min-width: 768px) {
@@ -114,8 +124,17 @@ export default {
 
     ion-icon {
       font-size: 27em;
+      @include fade-in-animation($duration: 1000ms, $delay: 800ms)
     }
   }
+}
 
+@keyframes from-left {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0%);
+  }
 }
 </style>
