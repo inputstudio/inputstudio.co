bbb10c41afba09b7bf1215a611fa64f4358303eb
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-24 02:34:54 +0000

    HomeContact section made

--- components/HomeContact.vue
+++ components/HomeContact.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="contact--wrapper">
     <div class="contact--title">
-      <h1>Contactez nous</h1>
+      <h1>Prêt à tenter une expérience inédite ?</h1>
     </div>
     <div class="contact--content">
-      <h2>
-        Faites nous confiance, soyez notre premier client ! 
-      </h2>
+      <h2>Faites nous confiance, soyez notre premier client !</h2>
     </div>
+    <nuxt-link to="/contact-us" class="contact-us">Contactez nous</nuxt-link>
   </div>
 </template>
 
@@ -19,5 +18,21 @@ export default {}
 .contact--wrapper {
   width: 100%;
   padding: 6rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2.5em;
 }
+
+.contact--title {
+  font-size: 1.5em;
+}
+
+.contact-us {
+  @include button($cursor: pointer);
+  width: 15%;
+}
+
 </style>
