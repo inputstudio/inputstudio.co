bc70777127dd8867f4a68bbc8566a13a2e734720
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-25 17:17:13 +0000

    HomeContact description text edited

--- components/HomeContact.vue
+++ components/HomeContact.vue
@@ -4,7 +4,7 @@
       <h1>Prêt à tenter une expérience inédite ?</h1>
     </div>
     <div class="contact--content">
-      <h2>Faites nous confiance, soyez notre premier client !</h2>
+      <h2>Faites nous confiance, faites partie de nos premiers clients !</h2>
     </div>
     <nuxt-link to="/contact-us" class="contact-us">Contactez nous</nuxt-link>
   </div>
