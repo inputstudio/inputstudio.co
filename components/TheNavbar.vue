cd827229394a48981b0ae1f2d7ccc6f3c336ca9b
Alex N'Guessan <alexmarcaureln@gmail.com>
2022-06-18 13:53:07 +0000

    Remove navlink hover effect on mobile

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar--wrapper">
-    <div class="navbar--brand">
+    <div class="navbar-brand">
       <TheLogo />
     </div>
 
-    <div class="navbar--links">
-      <nuxt-link to="/" class="navbar--link">Accueil</nuxt-link>
-      <nuxt-link to="/process" class="navbar--link">Démarche</nuxt-link>
-      <nuxt-link to="/about" class="navbar--link">À propos</nuxt-link>
-      <nuxt-link to="/contact" class="contact-btn">Nous contacter</nuxt-link>
+    <div class="navbar-links">
+      <nuxt-link to="/" class="navbar-link">Accueil</nuxt-link>
+      <nuxt-link to="/process" class="navbar-link">Démarche</nuxt-link>
+      <nuxt-link to="/about" class="navbar-link">À propos</nuxt-link>
+      <nuxt-link to="/contact" class="navbar-link contact-btn">Nous contacter</nuxt-link>
     </div>
   </nav>
 </template>
@@ -24,25 +24,24 @@ export default {}
   background: $bg-primary;
   color: $navbar-text-color;
   padding: 1rem;
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    gap: 1em;
-  }
+  flex-direction: column;
+  gap: 1em;
 
   @media screen and (min-width: 768px) {
+    flex-direction: row;
     align-items: center;
     padding: 1rem 20vw;
   }
 }
 
-@media screen and (max-width: 768px) {
-  .navbar--brand {
+.navbar-brand {
+  @media screen and (max-width: 768px) {
     display: flex;
     justify-content: center;
   }
 }
-.navbar--links {
+
+.navbar-links {
   display: flex;
 
   @media screen and (max-width: 768px) {
@@ -52,36 +51,36 @@ export default {}
   @media screen and (min-width: 768px) {
     align-items: center;
   }
-
-  a {
-    display: inline-block;
-    font-size: 1rem;
-
-    @media screen and (max-width: 768px) {
-      white-space: nowrap;
-      padding: 0.8rem;
-      margin: auto;
-    }
-
-    @media screen and (min-width: 768px) {
-      margin-inline: 1rem;
-    }
-  }
 }
 
-.navbar--link {
+.navbar-link {
+  display: inline-block;
+  font-size: 1rem;
   transition: border 0.1s ease-in-out;
-  padding-bottom: 3px;
 
-  &:hover {
-    padding-bottom: 0px;
-    border-bottom: 3px solid #fff;
+  @media screen and (max-width: 768px) {
+    white-space: nowrap;
+    padding: 0.5rem;
+    margin: auto;
+  }
+
+  @media screen and (min-width: 768px) {
+    margin-inline: 1rem;
+
+    &:hover {
+      border-bottom: 3px solid #fff;
+    }
   }
 }
 
 .contact-btn {
   @media screen and (min-width: 768px) {
     @include button($border: unset, $cursor: pointer);
+    margin-inline: 1rem;
+
+    &:hover {
+      border-bottom: unset;
+    }
   }
 }
 </style>
