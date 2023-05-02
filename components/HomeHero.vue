7efc3bd6ee221742f3584c9f09e7063d7b16b6f4
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-05-02 17:45:55 +0000

    Add animations on hero text

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="mb-12 flex flex-col items-center gap-14">
-    <h1 class="text-center text-9xl font-bold">Design. Code. Cloud.</h1>
+    <h1 id="animated-title" class="flex cursor-pointer gap-3 text-center text-9xl font-bold leading-normal">
+      <div id="design">Design.</div>
+      <div id="code">Code.</div>
+      <div id="cloud">Cloud.</div>
+    </h1>
     <p class="text-center text-2xl font-light md:w-1/2">
       Des idées créatives, une expertise technique et une infrastructure cloud de pointe. Notre agence web a tout ce
       dont vous avez besoin.
@@ -18,6 +22,7 @@
 </template>
 
 <script lang="ts" setup>
+import gsap from 'gsap';
 import awsLogo from '@/assets/img/aws.svg';
 import azureLogo from '@/assets/img/azure.svg';
 import doLogo from '@/assets/img/do.svg';
@@ -32,6 +37,21 @@ import reactLogo from '@/assets/img/react.svg';
 import sketchLogo from '@/assets/img/sketch.svg';
 import tailwindLogo from '@/assets/img/tailwind.svg';
 import vueLogo from '@/assets/img/vue.svg';
+
+useNuxtApp().hook('page:finish', () => {
+  const code = document.querySelector('#code');
+  const cloud = document.querySelector('#cloud');
+  const tl = gsap.timeline();
+  tl.from(code, { x: -100, ease: 'elastic.out', duration: 1 });
+  tl.to(cloud, { y: -100, autoAlpha: 0, ease: 'power1.out', duration: 2 });
+  tl.to(cloud, { y: 0, autoAlpha: 1, ease: 'power2.inOut' });
+
+  document.querySelector('#animated-title')?.addEventListener('mouseenter', () => {
+    if (!tl.isActive()) {
+      tl.play(0);
+    }
+  });
+});
 
 const technologies = [
   {
@@ -92,3 +112,25 @@ const technologies = [
   },
 ];
 </script>
+
+<style lang="scss">
+#design {
+  background: linear-gradient(90deg, #ff0000, #ff7300, #002bff, #7a00ff, #ff00c8, #ff0000);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 400% auto;
+  animation: gradient 30s linear infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0 0;
+  }
+  50% {
+    background-position: 400% 0;
+  }
+  100% {
+    background-position: 0 0;
+  }
+}
+</style>
