c85ed4f7394c71664721cef059261e118dbe3a3b
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-05-04 09:50:40 +0000

    Add career and privacy links in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,11 +2,17 @@
   <nav class="flex justify-between">
     <div class="flex gap-3">
       <a href="https://github.com/inputstudio/" target="_blank" rel="noopener noreferrer">
-        <Icon name="mdi:github" size="32px" />
+        <IconCSS name="mdi:github" size="32px" class="text-neutral-600 transition-colors hover:text-neutral-900" />
       </a>
       <a href="https://www.linkedin.com/company/inputstudio/" target="_blank" rel="noopener noreferrer">
-        <Icon name="mdi:linkedin" size="32px" />
+        <IconCSS name="mdi:linkedin" size="32px" class="text-neutral-600 transition-colors hover:text-neutral-900" />
       </a>
+      <nuxt-link to="#" title="Politique de confidentialité">
+        <IconCSS name="mdi:lock" size="32px" class="text-neutral-600 transition-colors hover:text-neutral-900" />
+      </nuxt-link>
+      <nuxt-link to="#" title="Offres d'emploi">
+        <IconCSS name="uim:bag" size="32px" class="text-neutral-600 transition-colors hover:text-neutral-900" />
+      </nuxt-link>
     </div>
     <nuxt-link to="#">Nous contacter</nuxt-link>
   </nav>
