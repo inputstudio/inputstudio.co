d6f6b257095965320ae32ebac23d2744c110b200
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-05-14 05:27:56 +0000

    Declutter contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,60 +1,33 @@
 <template>
-  <section class="rounded-md bg-gray-100 p-4 md:w-3/5">
+  <section class="flex flex-col gap-9 rounded-md bg-gray-100 p-6 md:w-3/5">
     <div class="flex flex-col gap-3">
-      <h1 class="text-4xl font-bold uppercase">Nous contacter !</h1>
-      <p>Discutons de votre projet, ensemble nous trouverons une solution aux tâches les plus difficiles.</p>
+      <h1 class="text-4xl font-bold uppercase">Nous contacter</h1>
+      <p>Remplissez ce formulaire et nous vous contacterons au plus vite pour discuter de votre projet.</p>
     </div>
 
-    <form
-      id="contact-form"
-      class="my-3 flex flex-col gap-5 border-t-2 border-gray-300 pb-1 pt-6"
-      @submit.prevent="submitForm"
-    >
-      <input type="text" placeholder="Votre nom*" :v-model="formData.nom" required />
+    <form id="contact-form" class="flex flex-col gap-3" @submit.prevent="submitForm">
+      <input type="text" placeholder="Votre nom" :v-model="data.fullname" required />
 
-      <div class="flex gap-4">
-        <input class="w-50" type="text" placeholder="E-mail/Contact*" :v-model="formData.contact" required />
+      <div class="flex gap-3">
+        <input class="w-1/2" type="text" placeholder="E-mail/Téléphone" :v-model="data.contact" required />
 
-        <select class="w-50" placeholder="Budget*" :v-model="formData.budget">
-          <option value="" selected disabled>Budget*</option>
+        <select class="w-1/2" :v-model="data.budget">
+          <option value="" selected disabled>Votre budget</option>
           <option v-for="budget in budgets" :key="budget" :value="budget">{{ budget }}</option>
         </select>
       </div>
 
-      <textarea
-        name=""
-        cols="30"
-        rows="10"
-        :v-model="formData.resume"
-        placeholder="Un bref résumé de votre projet*"
-      ></textarea>
+      <textarea rows="8" :v-model="data.resume" placeholder="Parlez-nous de votre projet"></textarea>
 
-      <div>
-        <input id="cahier-charge" type="file" :v-model="formData.cahierCharge" hidden />
+      <div class="mt-3">
+        <p>Selectionnez vos besoins :</p>
 
-        <label for="cahier-charge" class="flex w-full cursor-pointer items-center gap-4">
-          <span class="rounded-md bg-white p-2">
-            <img
-              :src="plus"
-              alt="Ajouter le cahier de charge"
-              title="Ajouter le cahier de charge"
-              width="15"
-              height="15"
-            />
-          </span>
-          Ajouter votre cahier de charge
-        </label>
-      </div>
-
-      <div class="border-t-2 border-gray-300 pt-4">
-        <p>Selectionner un sujet de contact</p>
-
-        <div class="my-2 flex flex-wrap gap-2">
+        <div class="my-3 flex flex-wrap gap-2">
           <p
             v-for="topic in topics"
             :key="topic"
             class="cursor-pointer rounded-full border border-black px-4 py-1 transition-colors hover:bg-black hover:text-white"
-            :class="{ 'bg-black text-white': selectedTopics.indexOf(topic) >= 0 }"
+            :class="{ 'bg-black text-white': data.topics.indexOf(topic) >= 0 }"
             @click="handleTopics(topic)"
           >
             {{ topic }}
@@ -62,60 +35,50 @@
         </div>
       </div>
 
-      <div class="border-t-2 border-gray-300 pt-4">
-        <p>En appuyant sur “Envoyer” vous acceptez sur notre politique de confidentialité.</p>
-
-        <button
-          type="submit"
-          class="mt-4 w-full rounded-lg border-2 bg-black py-4 text-center font-light capitalize text-white transition-colors hover:border-black hover:bg-white hover:text-black"
-        >
-          Envoyer
-        </button>
-      </div>
+      <button
+        type="submit"
+        class="mt-3 rounded-lg border-2 bg-black py-4 text-center font-semibold capitalize text-white transition-colors hover:border-black hover:bg-white hover:text-black"
+      >
+        Envoyer
+      </button>
     </form>
   </section>
 </template>
 
 <script lang="ts" setup>
-// @ts-nocheck 2307
-import plus from '@/assets/img/plus.svg';
-
-const formData = ref({
-  nom: '',
+const data = ref({
+  fullname: '',
   contact: '',
   budget: '',
   resume: '',
-  cahierCharge: null,
-  sujet: [],
+  topics: [] as string[],
 });
 
 const budgets = [
-  'Moins de 800 000 FCFA',
-  '800 000 - 1 500 000 FCFA',
-  '1 500 000 - 2 500 000 FCFA',
-  'plus de 2 500 000 FCFA',
+  'Moins de 1.000.000 FCFA',
+  '1.000.000 - 5.000.000 FCFA',
+  '5.000.000 - 10.000.000 FCFA',
+  'Plus de 10.000.000 FCFA',
 ];
 
 const topics = [
   'Site vitrine',
   'E-commerce',
+  'Site web sur mesure',
+  'Refonte de site web',
   'Application mobile',
-  'Cloud',
-  'Refonte site web',
-  'Sur mesure',
   'Design UI/UX',
+  'Cloud',
   'Autres',
 ];
 
-const selectedTopics = ref([]);
-
-function handleTopics(topic) {
-  const index = selectedTopics.value.indexOf(topic);
+function handleTopics(topic: string) {
+  const index = data.value.topics.indexOf(topic);
 
   if (index === -1) {
-    selectedTopics.value.push(topic);
+    data.value.topics.push(topic);
   } else {
-    selectedTopics.value.splice(index, 1);
+    data.value.topics.splice(index, 1);
   }
 }
 
@@ -127,7 +90,7 @@ async function submitForm() {}
   input,
   select,
   textarea {
-    border-radius: 10px;
+    border-radius: 0.375rem;
     padding: 12px 10px;
     background-color: white;
     color: gray;
--- pages/contact.vue
+++ pages/contact.vue
@@ -2,38 +2,35 @@
   <div>
     <AppBackButton />
 
-    <main class="flex h-full flex-col justify-center gap-5 px-8 py-16 md:flex-row md:px-24 md:py-24">
+    <main class="flex h-screen flex-col justify-center gap-6 px-9 py-16 md:flex-row md:p-24">
       <ContactForm />
 
-      <section class="flex flex-col rounded-md p-4">
+      <section class="flex flex-col">
         <h1 id="totb" class="relative mb-10 text-6xl uppercase md:text-8xl">THINK OUTSIDE THE BOX</h1>
 
         <div class="grid h-full w-full grid-cols-1 gap-5 md:grid-cols-2">
-          <div class="bg-gray-100 px-2">
-            <div class="border-b-2 border-gray-300 py-4 text-center">
-              <h4 class="text-lg font-bold capitalize">Carrières</h4>
+          <div class="rounded-md bg-gray-100 px-2">
+            <div class="py-9 text-center">
+              <h4 class="text-xl font-bold">Carrières</h4>
             </div>
 
-            <div class="flex justify-center md:h-full md:items-center">
-              <span class="capitalize">à très bientot</span>
+            <div class="flex flex-col p-3 md:h-full md:items-center">
+              <span>À très bientot</span>
             </div>
           </div>
 
-          <div class="bg-gray-100 px-2">
-            <div class="border-b-2 border-gray-300 py-4 text-center">
-              <h4 class="text-lg font-bold capitalize">Nos contacts</h4>
+          <div class="rounded-md bg-gray-100 px-2">
+            <div class="py-9 text-center">
+              <h4 class="text-xl font-bold">Nos contacts</h4>
             </div>
 
-            <div class="flex flex-col justify-center gap-3 px-4 md:h-full">
-              <div class="flex flex-col gap-2">
-                <p>Email :</p>
+            <div class="flex flex-col gap-3 p-3 md:h-full">
+              <p class="">
+                Vous détestez les formulaires ? <br />
+                Envoyez-nous un mail à
                 <a class="text-gray-600" href="mailto:hello@inputstudio.co">hello@inputstudio.co</a>
-              </div>
-
-              <div class="flex flex-col gap-2">
-                <p>Contact :</p>
-                <a class="text-gray-600" href="tel:+2250769561650">+225 07 69 56 16 50</a>
-              </div>
+              </p>
+              <p>Ou appelez-nous au : <a class="text-gray-600" href="tel:+2250769561650">+225 07 69 56 16 50</a></p>
 
               <div class="flex flex-col gap-2">
                 <p>Nous suivre :</p>
@@ -61,14 +58,13 @@
 </template>
 
 <script lang="ts" setup>
-// @ts-nocheck 230
 const calendly = useCalendly();
 const openCalendly = () => {
   calendly.showPopupWidget('https://calendly.com/input_studio/30min');
 };
 
 useHead({
-  title: 'Input Studio — Nous contacter',
+  title: 'Nous contacter — Input Studio',
 });
 </script>
 
