5a3cce461eff215a4b72e5e0d2bfc9919b02f2cd
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-06-11 20:58:21 +0000

    Add instagram to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,17 +1,17 @@
 <template>
   <nav class="flex justify-between">
     <div class="flex gap-3">
-      <a href="https://github.com/inputstudio/" target="_blank" rel="noopener noreferrer">
-        <IconCSS name="mdi:github" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+      <a href="https://instagram.com/input.studio" target="_blank" rel="noopener noreferrer" title="Instagram">
+        <IconCSS name="mdi:instagram" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
       </a>
 
-      <a href="https://www.linkedin.com/company/inputstudio/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/company/inputstudio" target="_blank" rel="noopener noreferrer" title="LinkedIn">
         <IconCSS name="mdi:linkedin" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
       </a>
 
-      <nuxt-link to="#" title="Politique de confidentialité">
-        <IconCSS name="mdi:lock" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
-      </nuxt-link>
+      <a href="https://github.com/inputstudio" target="_blank" rel="noopener noreferrer" title="GitHub">
+        <IconCSS name="mdi:github" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+      </a>
 
       <nuxt-link to="/carriere" title="Carrière">
         <IconCSS name="uim:bag" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
