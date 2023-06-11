62c093ab6dd7586839c040c015417c7769d39411
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-06-11 02:57:21 +0000

    homeghostcard animation on hover

--- components/HomeGhostCard.vue
+++ components/HomeGhostCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-between border border-white px-4 py-2">
+  <nuxt-link to="/contact" class="home-ghost-card flex items-center justify-between border border-white px-4 py-2">
     <div class="flex items-center gap-2">
       <Icon name="simple-icons:ghost" title="Logo ghost" size="2em" />
       <h2 class="text-2xl font-extralight">GHOST EXPERT</h2>
@@ -9,11 +9,43 @@
       <p class="text-md">Contacter-nous pour la création de votre blog</p>
       <img :src="rightArrow" alt="Right arrow" title="Right arrow" />
     </div>
-  </div>
+  </nuxt-link>
 </template>
 
 <script lang="ts" setup>
 import rightArrow from '@/assets/img/right-arrow.svg';
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.home-ghost-card {
+  $transition-duration: 500ms;
+
+  position: relative;
+  isolation: isolate;
+  transition: color $transition-duration;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    transform: scaleX(0);
+    transform-origin: left center;
+    transition: transform $transition-duration;
+    z-index: -1;
+  }
+
+  &:hover {
+    color: black;
+
+    &::before {
+      transform: scaleX(1);
+    }
+  }
+}
+</style>
