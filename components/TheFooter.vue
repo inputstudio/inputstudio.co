c02df06f2c609b930272f575eae313193dede95c
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-06-24 14:02:00 +0000

    Make footer icons brighter

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,19 +2,19 @@
   <nav class="flex justify-between">
     <div class="flex gap-3">
       <NuxtLink to="https://instagram.com/input.studio" target="_blank" title="Instagram">
-        <IconCSS name="mdi:instagram" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+        <IconCSS name="mdi:instagram" class="ball-expander text-3xl" />
       </NuxtLink>
 
       <NuxtLink to="https://www.linkedin.com/company/inputstudio" target="_blank" title="LinkedIn">
-        <IconCSS name="mdi:linkedin" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+        <IconCSS name="mdi:linkedin" class="ball-expander text-3xl" />
       </NuxtLink>
 
       <NuxtLink to="https://github.com/inputstudio" target="_blank" title="GitHub">
-        <IconCSS name="mdi:github" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+        <IconCSS name="mdi:github" class="ball-expander text-3xl" />
       </NuxtLink>
 
       <NuxtLink to="/carriere" title="Carrière">
-        <IconCSS name="uim:bag" size="32px" class="text-neutral-600 transition-colors hover:text-white" />
+        <IconCSS name="uim:bag" class="ball-expander text-3xl" />
       </NuxtLink>
     </div>
 
