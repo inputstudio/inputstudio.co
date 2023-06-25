c2082410807ea2f244b1cae9e61de65e9d8bb2e0
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-06-25 17:31:50 +0000

    AppExperienceText component

--- pages/carriere.vue
+++ pages/carriere.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="px-9 py-20 md:p-24">
     <p class="text-xl">Studio</p>
-    <h1 class="text-8xl font-bold">Nous ne réalisons pas de sites mais des expériences web.</h1>
+    <h1 class="text-8xl font-bold">
+      Nous ne réalisons pas de sites mais des
+      <AppExperienceText :text-class="'font-thin'" :circle-class="'-translate-y-8'" />.
+    </h1>
 
     <AppScrollIndicator />
 
