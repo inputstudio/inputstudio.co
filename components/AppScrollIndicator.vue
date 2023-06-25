68bf671dc401915164a4b20761deb0f539f4e9c8
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-06-25 02:08:16 +0000

    scroll indicator margin space fixed

--- components/AppScrollIndicator.vue
+++ components/AppScrollIndicator.vue
@@ -1,14 +1,10 @@
 <template>
-  <div class="mx-auto mt-36 flex w-max rotate-90 items-center justify-center gap-4">
+  <div class="mx-auto my-36 flex w-max rotate-90 items-center justify-center gap-4">
     <p class="uppercase">scroll</p>
     <div class="h-0.5 w-28 bg-white"></div>
   </div>
 </template>
 
-<script setup lang="ts">
-// const props = defineProps({
-//   class
-// })
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss"></style>
