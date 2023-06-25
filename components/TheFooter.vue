aef8248a2af32a234c4125d3ad46116dbb7f27ff
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-06-25 16:53:01 +0000

    default layout footer responsive

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="mx-24 flex flex-col justify-center gap-10 border-t border-white py-20">
-    <div class="flex justify-between">
+  <footer class="mx-10 flex flex-col justify-center gap-10 border-t border-white py-10 md:mx-24 md:py-20">
+    <div class="flex flex-col gap-8 text-center md:flex-row md:justify-between md:text-left">
       <div>
-        <h1 class="mb-2 text-6xl font-bold uppercase 2xl:text-8xl">INPUT STUDIO</h1>
-        <div class="flex gap-3">
+        <h1 class="mb-8 text-5xl font-bold uppercase md:mb-2 md:text-6xl 2xl:text-8xl">INPUT STUDIO</h1>
+        <div class="flex justify-center gap-6 md:justify-start md:gap-3">
           <NuxtLink
             v-for="(social, index) in socials"
             :key="`the-footer-social-${index}`"
@@ -12,8 +12,8 @@
             :title="social.title"
             class="flex items-center gap-2"
           >
-            <IconCSS :name="social.icon" class="text-2xl" />
-            <span>{{ social.text }}</span>
+            <IconCSS :name="social.icon" class="text-4xl md:text-2xl" />
+            <span class="hidden md:inline">{{ social.text }}</span>
           </NuxtLink>
         </div>
       </div>
@@ -26,7 +26,7 @@
       </div>
     </div>
 
-    <div class="flex justify-center gap-10 text-customGray">
+    <div class="flex justify-center text-center text-customGray md:gap-10 md:text-left">
       <a href="#" class="hover-underline">Politique de confidentialité</a>
       <span>&copy; Copyright (c) 2023</span>
     </div>
