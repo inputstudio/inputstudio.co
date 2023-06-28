0ca75d0c474a9b2f96d26289a2ac813fb17502f6
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-06-28 23:34:59 +0000

    fix!: move content to studio (where is belong)

--- pages/careers.vue
+++ pages/careers.vue
@@ -1,39 +1,5 @@
 <template>
-  <main class="flex flex-col gap-y-20 px-9 py-20 md:p-24">
-    <section>
-      <p class="text-xl">Studio</p>
-      <h1 class="text-8xl font-bold">
-        Nous ne réalisons pas de sites mais des
-        <AppExperienceText />.
-      </h1>
-
-      <AppScrollIndicator />
-    </section>
-
-    <section class="flex w-full items-center justify-center text-center">
-      <p class="text-2xl">
-        Input studio est une agence web <span class="font-bold">100% REMOTE</span> à Abidjan. Avec nous, construire un
-        site web est bien plus que simplement installer un logiciel de conception et faire de l'intégration.
-      </p>
-    </section>
-
-    <section class="flex w-full items-center justify-center text-center">
-      <p class="text-2xl">
-        En effet, notre équipe vous accompagne dans une démarche de planification, de choix technologique, de
-        développement et de maintenance web.
-      </p>
-    </section>
-
-    <section class="flex w-full items-center justify-center text-center">
-      <p class="text-2xl">
-        Notre agence web créative propose une approche complète, de la conception à la mise en ligne. Nous combinons des
-        compétences en design, en développement et en déploiement pour offrir à nos clients des sites web remarquables
-        qui reflètent leur identité, captivent leur public et atteignent leurs objectifs commerciaux.
-      </p>
-    </section>
-
-    <CareerActivities />
-
-    <CareerQualities />
-  </main>
+  <section></section>
 </template>
+
+<script lang="ts" setup></script>
