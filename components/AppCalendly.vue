f25f2e893105d7b2f501e0fdc18f8874043bcc65
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-06-28 19:41:23 +0000

    fix: make calendly responsive on its own

--- components/AppCalendly.vue
+++ components/AppCalendly.vue
@@ -1,19 +1,19 @@
 <template>
-  <button type="button" @click="openCalendly">
-    <div
-      class="transform rounded-2xl text-black shadow-lg md:absolute md:bottom-0 md:left-1/2 md:w-96 md:-translate-x-1/2 md:-translate-y-1/2"
-    >
-      <div class="flex items-center justify-center gap-3 rounded-t-2xl bg-white p-6">
-        <img src="@/assets/img/calendly.svg" alt="Logo Calendly" />
-        <div class="flex flex-col">
-          <span class="">Planifier une réunion</span>
-          <span class="font-thin">Rapide, pratique, efficace.</span>
-        </div>
+  <button
+    type="button"
+    class="absolute bottom-0 left-1/2 z-10 w-5/6 -translate-x-1/2 -translate-y-6 transform rounded-2xl text-black shadow-lg lg:w-96"
+    @click="openCalendly"
+  >
+    <div class="flex items-center justify-center gap-6 rounded-t-2xl bg-white p-6">
+      <img src="@/assets/img/calendly.svg" alt="Logo Calendly" />
+      <div class="flex flex-col text-left text-sm lg:text-base">
+        <span>Planifier une réunion</span>
+        <span class="font-thin">Rapide, pratique, efficace.</span>
       </div>
+    </div>
 
-      <div class="rounded-b-2xl bg-neutral-100">
-        <p class="py-2 text-center text-xs font-thin text-neutral-500">Copyright (c) {{ new Date().getFullYear() }}</p>
-      </div>
+    <div class="rounded-b-2xl bg-neutral-100">
+      <p class="py-2 text-center text-xs font-thin text-neutral-500">Copyright (c) {{ new Date().getFullYear() }}</p>
     </div>
   </button>
 </template>
