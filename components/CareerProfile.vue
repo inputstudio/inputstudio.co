975fb55aa37258afcd259e13e41015f874ed9b7b
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-06-29 21:29:28 +0000

    new career page and CareerProfile component

---
+++ components/CareerProfile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="space-y-4 border-b border-t border-white py-4">
+    <div class="flex cursor-pointer justify-between" @click="show = !show">
+      <h2 class="text-4xl">{{ props.title }}</h2>
+      <button type="button">
+        <IconCSS v-if="show" name="ic:baseline-minus" class="text-2xl" />
+        <IconCSS v-else name="ic:baseline-plus" class="text-2xl" />
+      </button>
+    </div>
+    <p v-show="show">
+      {{ props.description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const show = ref(props.isOpen);
+</script>
--- pages/careers.vue
+++ pages/careers.vue
@@ -1,5 +1,58 @@
 <template>
-  <section></section>
+  <main class="flex flex-col gap-y-20 px-9 py-20 md:p-24">
+    <section class="section">
+      <p class="text-xl">Carrière</p>
+      <h1 class="text-8xl font-bold">Nous embauchons</h1>
+
+      <div class="my-20 flex gap-20 text-justify">
+        <p class="w-2/5">
+          Input studio est toujours ouvert aux personnes intelligentes et créatives. Rejoignez notre équipe de personnes
+          partageant les mêmes idées qui s'efforcent d'atteindre leurs objectifs. Travaillons ensemble vers le succès !
+          Nous sommes intéressés par la construction d'une communauté d'individus qui partagent nos valeurs et nos
+          objectifs.
+        </p>
+
+        <div class="w-3/5">
+          <CareerProfile
+            v-for="(profile, index) in profiles"
+            :key="`career-profile-${index}`"
+            :position="index"
+            :is-open="index === 0"
+            :title="profile.title"
+            :description="profile.description"
+          />
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const profiles = [
+  {
+    title: 'Developpeur VueJS',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fuga repellat, illo itaque dicta accusamus obcaecati quis autem, architecto quod nisi quisquam assumenda ipsam maiores atque blanditiis, sapiente reprehenderit beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorem temporibus ducimus soluta sapiente exercitationem beatae, assumenda ipsa eius ad optio non enim nisi. Molestias delectus quos eos magnam quidem.',
+  },
+  {
+    title: 'Developpeur ReactJS',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fuga repellat, illo itaque dicta accusamus obcaecati quis autem, architecto quod nisi quisquam assumenda ipsam maiores atque blanditiis, sapiente reprehenderit beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorem temporibus ducimus soluta sapiente exercitationem beatae, assumenda ipsa eius ad optio non enim nisi. Molestias delectus quos eos magnam quidem.',
+  },
+  {
+    title: 'Developpeur React Native',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fuga repellat, illo itaque dicta accusamus obcaecati quis autem, architecto quod nisi quisquam assumenda ipsam maiores atque blanditiis, sapiente reprehenderit beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorem temporibus ducimus soluta sapiente exercitationem beatae, assumenda ipsa eius ad optio non enim nisi. Molestias delectus quos eos magnam quidem.',
+  },
+  {
+    title: 'Developpeur Dart/Flutter',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fuga repellat, illo itaque dicta accusamus obcaecati quis autem, architecto quod nisi quisquam assumenda ipsam maiores atque blanditiis, sapiente reprehenderit beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorem temporibus ducimus soluta sapiente exercitationem beatae, assumenda ipsa eius ad optio non enim nisi. Molestias delectus quos eos magnam quidem.',
+  },
+  {
+    title: 'Developpeur PHP',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut fuga repellat, illo itaque dicta accusamus obcaecati quis autem, architecto quod nisi quisquam assumenda ipsam maiores atque blanditiis, sapiente reprehenderit beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorem temporibus ducimus soluta sapiente exercitationem beatae, assumenda ipsa eius ad optio non enim nisi. Molestias delectus quos eos magnam quidem.',
+  },
+];
+</script>
