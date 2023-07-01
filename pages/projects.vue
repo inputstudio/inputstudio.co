e222e8c68a3ea96336ef2e572a1208fc10f62d90
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-07-01 14:12:56 +0000

    wip: project page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,26 @@
 <template>
-  <section></section>
+  <section class="section flex flex-col gap-10 px-9 py-20">
+    <div class="flex flex-col">
+      <p class="text-xl font-thin">Projets</p>
+      <h1 class="text-5xl font-bold md:text-8xl">Nos projets</h1>
+    </div>
+
+    <div class="my-10">
+      <div class="flex justify-between gap-20 text-center">
+        <h2 class="text-4xl">Notre travail, notre fierté</h2>
+        <p class="w-2/3">
+          Nous ne nous engageons que dans des projets qui reflètent nos valeurs profondes, et nous sommes passionnés par
+          une implication totale à chaque étape.
+        </p>
+      </div>
+    </div>
+
+    <div class="grid place-items-center">
+      <p class="text-center text-xl">Aucun projet à afficher pour le moment.</p>
+    </div>
+
+    <AppLetsStart />
+  </section>
 </template>
 
 <script lang="ts" setup></script>
