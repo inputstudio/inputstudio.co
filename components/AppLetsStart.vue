9dba2fc0eb61cc9cca84b4968e9e5fe972575e04
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-07-01 14:08:22 +0000

    reduce spacing btw elements

--- components/AppLetsStart.vue
+++ components/AppLetsStart.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section grid min-h-screen place-items-center">
-    <div class="flex flex-col items-center gap-y-7 text-center md:gap-y-10">
-      <h1 class="text-4xl leading-tight md:text-8xl">
+    <div class="flex flex-col items-center gap-y-3 text-center">
+      <h1 class="mb-6 text-4xl leading-tight md:text-8xl">
         Demarrons votre
         <AppExperienceText circle-class="-translate-x-4 -translate-y-20 md:-translate-y-10 md:-translate-x-2" />
         ensemble !
