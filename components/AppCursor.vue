85d3a85a94ef1a29ebb0d73f06ac7ff942309554
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-07-03 17:36:42 +0000

    fix(cursor): remove index offset and fetch hoverables on page change

--- components/AppCursor.vue
+++ components/AppCursor.vue
@@ -11,7 +11,6 @@ import { gsap } from 'gsap';
 
 const { hook } = useNuxtApp();
 const { isMobileOrTablet } = useDevice();
-const currentRoute = useRoute();
 
 const cursor = ref(null);
 
@@ -19,8 +18,8 @@ const expandableElements = 'a, .cursor-expander';
 
 function onMouseMove(event, cursor) {
   gsap.to(cursor, {
-    x: currentRoute.name === 'index' ? event.clientX - 35 : event.clientX,
-    y: currentRoute.name === 'index' ? event.clientY - 35 : event.clientY,
+    x: event.clientX,
+    y: event.clientY,
     duration: 0.4,
   });
 }
@@ -45,37 +44,21 @@ function init() {
   hoverables.forEach((element) => {
     element.addEventListener('mouseenter', () => onMouseEnter(cursor.value));
     element.addEventListener('mouseleave', () => onMouseLeave(cursor.value));
+    element.addEventListener('click', () => onMouseLeave(cursor.value));
   });
 
   document.body.addEventListener('mousemove', (e) => onMouseMove(e, cursor.value));
-}
 
-function clean() {
-  const hoverables = document.querySelectorAll(expandableElements);
-
-  hoverables.forEach((element) => {
-    element.removeEventListener('mouseenter', () => onMouseEnter(cursor.value));
-    element.removeEventListener('mouseleave', () => onMouseLeave(cursor.value));
-  });
-
-  document.body.removeEventListener('mousemove', (e) => onMouseMove(e, cursor.value));
+  if (cursor.value) {
+    cursor.value.classList.remove('hidden');
+  }
 }
 
 hook('page:finish', () => {
-  if (!cursor.value) {
-    return;
-  }
-
   init();
-  cursor.value.classList.remove('hidden');
 });
 
-hook('page:start', () => {
-  if (!cursor.value) {
-    return;
-  }
-
-  clean();
-  cursor.value.classList.add('hidden');
+hook('page:transition:finish', () => {
+  init();
 });
 </script>
