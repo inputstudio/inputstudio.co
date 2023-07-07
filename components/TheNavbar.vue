f824b59123e5e407275ca4f3230244df46245c5f
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-07-07 18:19:25 +0000

    chore: remove todo

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,11 +1,12 @@
 <template>
-  <nav class="absolute top-6 flex w-screen justify-between px-9">
+  <nav class="absolute top-6 flex w-screen items-center justify-between px-9">
     <NuxtLink to="/" class="flex items-center gap-1 text-gray-500 transition-colors hover:text-white">
       <IconCSS name="ic:baseline-arrow-outward" class="-rotate-90" />
       <span>Retour à l'accueil</span>
     </NuxtLink>
 
-    <!-- TODO: Add a glow effect on hover or something else -->
-    <span class="hidden rounded-full border-[1px] border-white px-3 py-1 uppercase lg:block">Agence 100% remote</span>
+    <div class="hidden w-fit rounded-full border-[1px] border-white px-6 py-3 uppercase md:block">
+      Agence 100% remote
+    </div>
   </nav>
 </template>
