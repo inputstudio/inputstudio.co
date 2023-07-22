717e98b2344c1be97bf29bb2cf84deb4ee3c8dc3
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-07-22 19:31:45 +0000

    chore(project): fade animation added to project page elements

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col gap-y-20 py-20">
+  <div ref="wrapper" class="flex flex-col gap-y-20 py-20">
     <div class="flex flex-col px-9">
-      <p class="text-xl font-thin">Projets</p>
-      <h1 class="text-5xl font-bold md:text-8xl">Nos projets</h1>
+      <p ref="title" class="text-xl font-thin">Projets</p>
+      <h1 ref="subtitle" class="text-5xl font-bold md:text-8xl">Nos projets</h1>
     </div>
 
     <div class="p-9">
       <div class="flex flex-col justify-between gap-9 md:flex-row md:gap-20 md:text-center">
-        <h2 class="text-4xl">Notre travail, notre fierté</h2>
-        <p class="md:w-2/3">
+        <h2 ref="slogan" class="text-4xl">Notre travail, notre fierté</h2>
+        <p ref="description" class="md:w-2/3">
           Nous ne nous engageons que dans des projets qui reflètent nos valeurs profondes, et nous sommes passionnés par
           une implication totale à chaque étape.
         </p>
@@ -25,6 +25,21 @@
 
 <script lang="ts" setup>
 const { projects } = useStore();
+const { hook } = useNuxtApp();
+
+const wrapper = ref();
+const title = ref();
+const subtitle = ref();
+const slogan = ref();
+const description = ref();
+
+const setupTimeline = () => {
+  fadeAnimation(wrapper.value, [title.value, subtitle.value, slogan.value, description.value]);
+};
+
+/** To Do : should be trigger on page refresh or on page change !!! */
+hook('page:transition:finish', setupTimeline);
+hook('page:finish', setupTimeline);
 
 useHead({
   title: 'Projets',
