815fe47c27993511a6c4a7c728946c397078232c
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-07-24 10:02:02 +0000

    chore: fix lint and format issues

--- pages/index.vue
+++ pages/index.vue
@@ -15,14 +15,21 @@ useHead({
 
 <style lang="scss" scoped>
 main {
-  background: no-repeat center url('@/assets/img/halo-green.webp'), no-repeat center url('@/assets/img/halo-blue.webp');
-  background-position: top -5% right 25%, bottom 0 left 15%;
+  background:
+    no-repeat center url('@/assets/img/halo-green.webp'),
+    no-repeat center url('@/assets/img/halo-blue.webp');
+  background-position:
+    top -5% right 25%,
+    bottom 0 left 15%;
   background-color: black;
 
   @media screen and (min-width: 768px) {
-    background: no-repeat center url('@/assets/img/halo-green.webp'),
+    background:
+      no-repeat center url('@/assets/img/halo-green.webp'),
       no-repeat center/75% url('@/assets/img/halo-blue.webp');
-    background-position: top 0% left -45%, bottom 0% right -45%;
+    background-position:
+      top 0% left -45%,
+      bottom 0% right -45%;
   }
 }
 </style>
