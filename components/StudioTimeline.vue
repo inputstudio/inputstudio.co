0253698c8888f35e7209b6b6dd9d28cb12f79b55
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-07-28 19:38:04 +0000

    feat(i18n): add studio timeline tranduction

--- components/StudioTimeline.vue
+++ components/StudioTimeline.vue
@@ -1,16 +1,14 @@
 <template>
   <section ref="wrapper" class="flex flex-col gap-10">
-    <h1 ref="title" class="text-6xl font-medium">Notre processus</h1>
-    <p ref="description" class="text-lg font-light md:w-3/4 md:text-2xl">
-      Nous nous chargeons de la gestion de votre projet, de sa phase de lancement jusqu'à son déploiement.
-    </p>
+    <h1 ref="title" class="text-6xl font-medium">{{ $t('studio.timeline.title') }}</h1>
+    <p ref="description" class="text-lg font-light md:w-3/4 md:text-2xl">{{ $t('studio.timeline.description') }}</p>
 
     <div class="grid grid-cols-1 pt-10 md:pt-0">
       <StudioTimelineItem
         class="timeline-item"
-        title="Planifier une réunion"
-        description="Nous avons une réunion pour définir la portée du projet et savoir quelles sont vos attentes"
-        button-text="Planifier une reunion"
+        :title="$t('calendly.plan-meeting')"
+        :description="$t('calendly.purpose')"
+        :button-text="$t('calendly.plan-meeting')"
         :button-action="openCalendly"
       />
 
@@ -26,6 +24,7 @@
 </template>
 
 <script lang="ts" setup>
+const { t } = useI18n();
 const { $gsap, $ScrollTrigger } = useNuxtApp();
 const { openCalendly } = useCalendlyWidget();
 
@@ -33,19 +32,16 @@ $gsap.registerPlugin($ScrollTrigger);
 
 const items = [
   {
-    title: 'Design',
-    description:
-      'Nos concepteurs rédigent soigneusement la première version pour votre examen qui passe ensuite par autant de révisions que nécessaire.',
+    title: t('studio.timeline.design'),
+    description: t('studio.timeline.design-desc'),
   },
   {
-    title: 'Intégration',
-    description:
-      "Notre équipe technique possède une expertise approfondie dans le domaine de l'intégration et utilise les meilleures pratiques pour garantir des intégrations fluides, sécurisées et évolutives. Nous veillons à ce que toutes les composantes de votre écosystème numérique fonctionnent harmonieusement ensemble, offrant une expérience utilisateur cohérente et une gestion efficace des données.",
+    title: t('studio.timeline.integration'),
+    description: t('studio.timeline.integration-desc'),
   },
   {
-    title: 'Livraison du projet',
-    description:
-      'Nous mettons tout en œuvre pour respecter les délais convenus et livrer votre projet dans les délais prévus, tout en maintenant la qualité et la satisfaction du client au cœur de notre travail.',
+    title: t('studio.timeline.delivery'),
+    description: t('studio.timeline.delivery-desc'),
   },
 ];
 
