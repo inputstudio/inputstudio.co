99eec3ed5239e879775821e783a8930978dd5a4a
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-12-12 15:34:35 +0000

    fix: update CalendlyWidget appearance

--- components/CalendlyWidget.vue
+++ components/CalendlyWidget.vue
@@ -1,17 +1,17 @@
 <template>
   <button
     type="button"
-    class="cursor-expander absolute bottom-0 left-1/2 w-max -translate-x-1/2 -translate-y-5 transform rounded-2xl text-black shadow-lg lg:w-96"
+    class="cursor-expander absolute bottom-0 left-1/2 w-max -translate-x-1/2 -translate-y-5 transform rounded-2xl text-black shadow-lg lg:w-80"
     @click="openCalendly"
   >
-    <div class="flex items-center justify-center gap-6 rounded-t-2xl bg-white p-3 md:p-4 2xl:p-6">
+    <div class="flex items-center justify-center gap-3 rounded-t-2xl bg-white p-3 md:p-4 2xl:p-6">
       <picture>
         <source srcset="/img/calendly.svg" alt="Logo Calendly" media="(min-width: 768px)" width="64" height="64" />
         <img src="/img/calendly.svg" alt="Logo Calendly" width="48" height="48" />
       </picture>
       <div class="flex flex-col text-left text-sm md:text-base">
         <span>{{ $t('calendly.plan-meeting') }}</span>
-        <span class="font-thin">{{ $t('calendly.advantages') }}</span>
+        <span class="font-light">{{ $t('calendly.advantages') }}</span>
       </div>
     </div>
 
