00116c1af4093e99fe736180fb1b1271fbb37fc4
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-12-20 13:25:24 +0000

    chore: upgrade prettier and load tailwind plugin

--- components/AppLoader.vue
+++ components/AppLoader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-auto">
     <svg
-      class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      class="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
