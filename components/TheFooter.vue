71d66df4f9234a8f8f3e7ecce4792e5dc7f2dbe6
Alex N'Guessan <alexmarcaureln@gmail.com>
2024-06-08 21:46:38 +0000

    refactor: update footer layout and remove unnecessary elements

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="mx-9 flex flex-col justify-center gap-10 border-t border-white py-9 md:py-12">
+  <footer class="mx-9 flex flex-col justify-center gap-10 border-t-[0.5px] border-white py-9 md:py-12">
     <div class="flex flex-col gap-8 text-center md:flex-row md:justify-between md:text-left">
       <div>
         <NuxtLink to="/">
-          <img src="/img/logo.webp" alt="Logo" class="h-18 -translate-y-4 md:block md:h-32 md:-translate-x-9" />
+          <img src="/img/logo.webp" alt="Logo" class="h-18 -translate-y-4 md:block md:h-20 md:-translate-x-6" />
         </NuxtLink>
         <div class="flex justify-center gap-6 md:justify-start md:gap-3">
           <NuxtLink
@@ -15,7 +15,7 @@
             class="flex items-center gap-2"
           >
             <IconCSS :name="social.icon" class="text-4xl md:text-2xl" />
-            <span class="hidden md:inline">{{ social.text }}</span>
+            <span class="hidden text-sm md:inline">{{ social.title }}</span>
           </NuxtLink>
         </div>
       </div>
@@ -34,17 +34,7 @@
       </div>
     </div>
 
-    <div class="flex flex-col items-center justify-center gap-3 text-center text-customGray md:text-left">
-      <div class="flex items-center justify-center gap-1">
-        <NuxtLink to="/privacy">{{ $t('navigation.privacy') }}</NuxtLink>
-        <span>·</span>
-        <p>Copyright &copy; 2022 - {{ new Date().getFullYear() }}</p>
-      </div>
-      <div class="flex items-center justify-center gap-1">
-        <span>powered by</span>
-        <IconCSS name="line-md:coffee-loop" class="text-xl" />
-      </div>
-    </div>
+    <p class="text-center text-customGray">Copyright &copy; 2022 - {{ new Date().getFullYear() }}</p>
   </footer>
 </template>
 
